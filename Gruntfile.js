60de53a982e27d14f23c63df2598e61c4d88606d
hello2mao <mhb@M.local>
2019-02-03 22:45:13 +0800

    Lazy push: 2019年 2月 3日 星期日 22时45分13秒 CST

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,37 +3,35 @@ module.exports = function(grunt) {
     // Project configuration.
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
-        concat: {
-            options: {
-            },
-            dist: {
-                src: ['js/plug-in.js', 'js/beautify.js', 'js/structure.js'],
-                dest: 'js/<%= pkg.name %>.min.js'
-            }
-        },
         uglify: {
             main: {
-                src: 'js/<%= pkg.name %>.min.js',
+                src: 'js/<%= pkg.name %>.js',
                 dest: 'js/<%= pkg.name %>.min.js'
             }
         },
         less: {
+            expanded: {
+                options: {
+                    paths: ["css"]
+                },
+                files: {
+                    "css/<%= pkg.name %>.css": "less/<%= pkg.name %>.less"
+                }
+            },
             minified: {
                 options: {
                     paths: ["css"],
                     cleancss: true
                 },
                 files: {
-                    "css/<%= pkg.name %>.min.css": "less/<%= pkg.name %>.less",
-                    "css/night-mode.min.css": "less/night-mode.less",
-                    "css/daily-mode.min.css": "less/daily-mode.less"
+                    "css/<%= pkg.name %>.min.css": "less/<%= pkg.name %>.less"
                 }
             }
         },
         banner: '/*!\n' +
-                ' * <%= pkg.title %> v<%= pkg.version %> (<%= pkg.homepage %>)\n' +
-                ' * Copyright <%= grunt.template.today("yyyy") %> <%= pkg.author %>\n' +
-                ' */\n',
+            ' * <%= pkg.title %> v<%= pkg.version %> (<%= pkg.homepage %>)\n' +
+            ' * Copyright <%= grunt.template.today("yyyy") %> <%= pkg.author %>\n' +
+            ' */\n',
         usebanner: {
             dist: {
                 options: {
@@ -41,36 +39,35 @@ module.exports = function(grunt) {
                     banner: '<%= banner %>'
                 },
                 files: {
-                    src: ['css/*.min.css', 'js/<%= pkg.name %>.min.js']
+                    src: ['css/<%= pkg.name %>.css', 'css/<%= pkg.name %>.min.css', 'js/<%= pkg.name %>.min.js']
                 }
             }
         },
         watch: {
             scripts: {
-                files: ['js/*.js'],
-                tasks: ['concat', 'uglify'],
+                files: ['js/<%= pkg.name %>.js'],
+                tasks: ['uglify'],
                 options: {
-                    spawn: false
-                }
+                    spawn: false,
+                },
             },
             less: {
                 files: ['less/*.less'],
                 tasks: ['less'],
                 options: {
-                    spawn: false
+                    spawn: false,
                 }
-            }
-        }
+            },
+        },
     });
 
     // Load the plugins.
     grunt.loadNpmTasks('grunt-contrib-uglify');
-    grunt.loadNpmTasks('grunt-contrib-concat');
     grunt.loadNpmTasks('grunt-contrib-less');
     grunt.loadNpmTasks('grunt-banner');
     grunt.loadNpmTasks('grunt-contrib-watch');
 
     // Default task(s).
-    grunt.registerTask('default', ['concat', 'uglify', 'less', 'usebanner']);
+    grunt.registerTask('default', ['uglify', 'less', 'usebanner']);
 
 };
