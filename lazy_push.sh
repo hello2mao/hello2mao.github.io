00830e4c0daded9aeb1b2fbc30fac37a3b39a1a7
hello2mao <hello2mao@gmail.com>
2019-12-05 22:01:07 +0800

    Lazy push: 2019年12月 5日 星期四 22时01分06秒 CST

--- lazy_push.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-date=`date`
-
-git add .
-git commit -m "Lazy push: $date"
-git push
--- snippet/rust/native-thread/target/debug/native-thread.d
+++
@@ -1,1 +0,0 @@
-/Users/mhb/github.com/hello2mao/hello2mao.github.io/snippet/rust/native-thread/target/debug/native-thread: /Users/mhb/github.com/hello2mao/hello2mao.github.io/snippet/rust/native-thread/src/main.rs
