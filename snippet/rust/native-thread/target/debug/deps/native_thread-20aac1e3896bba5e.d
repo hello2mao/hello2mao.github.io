284267d2f24fd8c72a1db6e1f9f3e3b8f4013903
hello2mao <hello2mao@gmail.com>
2019-12-05 21:59:40 +0800

    Lazy push: 2019年12月 5日 星期四 21时59分40秒 CST

---
+++ snippet/rust/native-thread/target/debug/deps/native_thread-20aac1e3896bba5e.d
@@ -0,0 +1,5 @@
+/Users/mhb/github.com/hello2mao/hello2mao.github.io/snippet/rust/native-thread/target/debug/deps/native_thread-20aac1e3896bba5e: src/main.rs
+
+/Users/mhb/github.com/hello2mao/hello2mao.github.io/snippet/rust/native-thread/target/debug/deps/native_thread-20aac1e3896bba5e.d: src/main.rs
+
+src/main.rs:
---
+++ snippet/rust/native-thread/target/debug/native-thread.d
@@ -0,0 +1,1 @@
+/Users/mhb/github.com/hello2mao/hello2mao.github.io/snippet/rust/native-thread/target/debug/native-thread: /Users/mhb/github.com/hello2mao/hello2mao.github.io/snippet/rust/native-thread/src/main.rs
